72173164722ae16273fa54e049626476f92d9c50
Al Nasimul Haque <alnasimul@gmail.com>
2021-05-26 23:22:47 +0600

    basic setup done

---
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import chair from '../../../images/chair.png';
+
+const HeaderMain = () => {
+    return (
+        <main className="row">
+            <div className="col-md-4 offset-md-1">
+                <h1>Your New Smile <br/> Starts Here</h1>
+                <p className="text-secondary"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione, quisquam accusamus autem eum excepturi nesciunt.</p>
+                <button className="btn btn-success">GET APPOINTMENT</button>
+            </div>
+            <div className="col-md-6">
+                <img src={chair} alt="" className="img-fluid"/>
+            </div>
+        </main>
+    );
+};
+
+export default HeaderMain;
