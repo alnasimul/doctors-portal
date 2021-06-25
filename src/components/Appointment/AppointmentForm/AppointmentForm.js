4b4f6c6dd1898d9be464a56c9140236757d8e3f8
Al Nasimul Haque <alnasimul@gmail.com>
2021-06-25 20:29:36 +0600

    authentication system added

--- src/components/Appointment/AppointmentForm/AppointmentForm.js
+++ src/components/Appointment/AppointmentForm/AppointmentForm.js
@@ -18,8 +18,27 @@ Modal.setAppElement('#root')
 const AppointmentForm = ({ modalIsOpen, closeModal, appointmentOn, date}) => {
     const { register, handleSubmit,formState: { errors }   } = useForm();
 const onSubmit = data => {
+    data.service = appointmentOn;
+    data.date = date.toDateString();
+    data.created = new Date().toDateString();
+
+    // fetch('http://localhost:5000/addAppointment',{
+    //     method: 'POST',
+    //     headers: {
+    //         'content-type' : 'application/json'
+    //     },
+    //     body: JSON.stringify(data)
+    // })
+    // .then(res => res.json())
+    // .then( success => {
+    //     if(success){
+    //         closeModal();
+    //         alert('Thanks for creating appointment, your appointment created successfully On ' + data.date);
+    //     }
+    // } )
+
     console.log(data)
-    closeModal();
+   
 };
     return (
         <div>
---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDpInfSNydUv4uekfrJpCBt-knUG0lZnEQ",
+    authDomain: "doctors-portal-01-6259c.firebaseapp.com",
+    projectId: "doctors-portal-01-6259c",
+    storageBucket: "doctors-portal-01-6259c.appspot.com",
+    messagingSenderId: "719434756386",
+    appId: "1:719434756386:web:5ecf4295bbc1a23a0ab57b"
+  };
+
+export default firebaseConfig;
